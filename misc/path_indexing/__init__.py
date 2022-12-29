b51e223890ec25b522e7d7db3dfea2ee34558870
Aidan Courtney <aidanfc97@gmail.com>
2022-12-28 21:59:11 -0500

    Restructure & fmt.

---
+++ misc/path_indexing/__init__.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import os
+import re
+
+
+def scan(root):
+    dirs = [root]
+    pop = dirs.pop
+    append = dirs.append
+    scan = os.scandir
+    is_dir = os.DirEntry.is_dir
+    while dirs:
+        for entry in scan(pop()):
+            path = entry.path
+            yield path
+            if is_dir(entry, follow_symlinks=False):
+                append(path)
+
+
+def scan_safe(root):
+    dirs = [root]
+    pop = dirs.pop
+    append = dirs.append
+    scan = os.scandir
+    is_dir = os.DirEntry.is_dir
+    while dirs:
+        try:
+            for entry in scan(pop()):
+                path = entry.path
+                yield path
+                try:
+                    if is_dir(entry, follow_symlinks=False):
+                        append(path)
+                except OSError:
+                    continue
+        except OSError:
+            continue
+
+
+def make_index(
+    root: str = "/", path: str = Path.home() / "fspaths.index", scan=scan_safe
+):
+    with open(path, "w") as file:
+        for i, entry in enumerate(scan(root)):
+            file.write(entry + "\n")
+    return i
+
+
+def search_index(pat, path=Path.home() / "fspaths.index"):
+    pat = re.compile(pat)
+    with open(path) as file:
+        yield from (ln for ln in file if pat.match(ln))
+
+
+def search(pat):
+    try:
+        return list(search_index(pat))
+    except:
+        make_index()
+        return list(search_index(pat))
